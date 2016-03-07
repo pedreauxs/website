047a478357f2e5cd1f53ec44312fd23f82db03d5
James Benner <james.benner@gmail.com>
2016-03-06 20:49:01 -0500

    Added email sign-up form.

---
+++ resources/views/thank-you.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pedreaux's</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <img src="images/logo.png" alt="Pedreaux’s">
+        <p>Thank you for your interest in Pedreaux’s. You will receive updates on our progress at it becomes available.</p>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <meta charset="UTF-8">
+        <title>Pedreaux's</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="css/app.css" rel="stylesheet">
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <img src="images/logo.png" alt="Pedreaux’s">
+                <h3>COMING SOON TO HOUSTON!</h3>
+                <p>If you're interested in our progress sign up below.</p>
+                <form action="/sign-ups" method="POST">
+                    {{ csrf_field() }}
+                    <p class="input">
+                        <input type="text" class="button" name="email" placeholder="NAME@EXAMPLE.COM" required>
+                        <input type="submit" class="button" value="SIGN UP">
+                    </p>
+                </form>
             </div>
         </div>
     </body>
