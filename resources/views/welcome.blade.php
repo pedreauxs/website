fe98d2d9087018396764d44fe7b731b08e597830
James Benner <james.benner@gmail.com>
2016-03-09 19:41:10 -0500

    Mobile view responsive fix.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+    <head itemscope itemtype="http://schema.org/WebSite">
         <meta charset="UTF-8">
-        <title>Pedreaux's</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-        <link href="css/app.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title itemprop="name">Pedreaux's</title>
+        <link rel="canonical" itemprop="url" href="http://pedreauxs.com/">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+        <link rel="stylesheet" href="css/app.css">
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <img src="images/logo.png" alt="Pedreaux’s">
+                <img class="logo" src="images/logo.png" alt="Pedreaux’s">
                 <h3>COMING SOON TO HOUSTON!</h3>
                 <p>If you're interested in our progress sign up below.</p>
                 <form action="/sign-ups" method="POST">
