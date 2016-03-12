ebe7dac543d7281052606bde91f704a5a919e530
James Benner <james.benner@gmail.com>
2016-03-12 16:32:14 -0500

    Potential fix for the sign-up page rendering bug.

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Pedreaux's</title>
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link href="css/app.css" rel="stylesheet">
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <img src="images/logo.png" alt="Pedreaux’s">
-        <p>Thank you for your interest in Pedreaux’s. You will receive updates on our progress as it becomes available.</p>
-    </div>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Pedreaux's</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <img class="logo" src="images/logo.png" alt="Pedreaux’s">
+                <p>Thank you for your interest in Pedreaux’s. You will receive updates on our progress as it becomes available.</p>
+            </div>
+        </div>
+    </body>
 </html>
